41498abd1d83f5e245bb9dabb72e454b1a3c54af
natalkalink <112897138+natalkalink@users.noreply.github.com>
2023-11-03 10:26:00 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict"
+document.addEventListener("click", function (e) {
+	const targetElement = e.target;
+
+	if (targetElement.closest('.menu-icon')) {
+		document.documentElement.classList.toggle('menu-open');
+		e.preventDefault();
+	}
+
+})
